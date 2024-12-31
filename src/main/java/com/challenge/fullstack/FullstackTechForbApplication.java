cd872460f10cfe00cc5263ae4e8bd77874c2c830
zotel1 <zotelsigel@gmail.com>
2024-12-31 15:20:53 -0300

    Initial commit

---
+++ src/main/java/com/challenge/fullstack/FullstackTechForbApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackTechForbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackTechForbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/fullstack/FullstackTechForbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTechForbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
