30cd1341f74bfde1b369fc7d1cd0ea79c8bce32e
zotel1 <zotelsigel@gmail.com>
2025-01-02 14:03:04 -0300

    add, created service JwtUtilService

--- src/main/java/com/challenge/fullstack/service/JwtUtilService.java
+++ src/main/java/com/challenge/fullstack/service/JwtUtilService.java
@@ -3,17 +3,19 @@ package com.challenge.fullstack.service;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.function.Function;
 
 @Service
 public class JwtUtilService {
 
     private static final String JWT_SECRET_KEY = "$2a$12$IfwkjkkPnPIwxSzWxBzHAOjcHwzgr9MQf60FbP78WxIR.S7cxSJOi";
 
+    // Tiempo de validez del token
     private static final long JWT_TIME_VALIDITY = 1000 * 60 * 15;
 
     public String generateToken(UserDetails userDetails) {
@@ -26,4 +28,21 @@ public class JwtUtilService {
                 .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
                 .compact();
     }
+
+    // Validez del token del frontend
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return extractClaim(token, Claims::getSubject).equals(userDetails.getUsername())
+                && !extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsTFunction) {
+        Claims claims = Jwts.parser().setSigningKey(JWT_SECRET_KEY).build().parseClaimsJws(token).getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
 }
