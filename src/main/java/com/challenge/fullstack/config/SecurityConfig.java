4bdea817a34c940fbbcf40f516b58acc1ad89e72
zotel1 <zotelsigel@gmail.com>
2025-01-02 17:22:56 -0300

    jwt, funcionando, debo agregar las tablas de mis base de datos de plantas y la conexion a la misma

--- src/main/java/com/challenge/fullstack/config/SecurityConfig.java
+++ src/main/java/com/challenge/fullstack/config/SecurityConfig.java
@@ -1,28 +1,37 @@
 package com.challenge.fullstack.config;
 
+import com.challenge.fullstack.filter.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
     @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
-        http.cors(withDefaults())
+        http
+                .cors(withDefaults())
                 .csrf(crf -> crf.disable())
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
@@ -34,6 +43,7 @@ public class SecurityConfig {
         return  new BCryptPasswordEncoder();
     }
 
+    @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
