110b67340446b3d0df69a9abd0dd9cbe059f5b7f
zotel1 <zotelsigel@gmail.com>
2025-01-01 21:13:22 -0300

    add repository, PlantRepository

--- src/main/java/com/challenge/fullstack/repository/PlantRepository.java
+++ src/main/java/com/challenge/fullstack/repository/PlantRepository.java
@@ -1,7 +1,22 @@
 package com.challenge.fullstack.repository;
 
+import com.challenge.fullstack.model.PlantModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class PlantRepository {
+public class PlantRepository implements  IPlantRepository{
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<PlantModel> findAll() {
+        String SQL = "SELECT * FROM plants";
+        return  jdbcTemplate.query(SQL, BeanPropertyRowMapper.newInstance(PlantModel.class));
+    }
 }
