7255703b06f4b62c3e605d70a163c4de17c90b86
zotel1 <zotelsigel@gmail.com>
2025-01-01 21:15:25 -0300

    add service, IPlantService

---
+++ src/main/java/com/challenge/fullstack/service/IPlantService.java
@@ -0,0 +1,9 @@
+package com.challenge.fullstack.service;
+
+import com.challenge.fullstack.model.PlantModel;
+
+import java.util.List;
+
+public interface IPlantService {
+    public List<PlantModel> findAll();
+}
