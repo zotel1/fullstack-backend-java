c6a13d13d5e3dc19585b3b90063fe6bf18a92fef
zotel1 <zotelsigel@gmail.com>
2025-01-02 13:00:50 -0300

    add, implementamos el UserDetailsServiceImpl

--- src/main/java/com/challenge/fullstack/service/UserDetailsServiceImpl.java
+++ src/main/java/com/challenge/fullstack/service/UserDetailsServiceImpl.java
@@ -1,16 +1,31 @@
 package com.challenge.fullstack.service;
 
+import com.challenge.fullstack.model.UserModel;
+import com.challenge.fullstack.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository iUserRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //Llamada a la base de datos
+        UserModel userModel = this.iUserRepository.findByName(username);
 
-        return null;
+        if(userModel == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new User(userModel.getName(), userModel.getPassword(), new ArrayList<>());
     }
 }
