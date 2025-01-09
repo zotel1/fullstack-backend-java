35897e29b803e2d99c7f67f07df64ab53e71441e
zotel1 <zotelsigel@gmail.com>
2025-01-09 18:03:54 -0300

    update PlantController postMapping

--- src/main/java/com/challenge/fullstack/controller/PlantController.java
+++ src/main/java/com/challenge/fullstack/controller/PlantController.java
@@ -1,28 +1,38 @@
 package com.challenge.fullstack.controller;
 
 import com.challenge.fullstack.model.PlantModel;
-import com.challenge.fullstack.service.IPlantService;
+import com.challenge.fullstack.service.PlantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @CrossOrigin("*")
+@RestController
 @RequestMapping("api/v1")
 public class PlantController {
 
     @Autowired
-    IPlantService iPlantService;
+    PlantService plantService;
 
-    @GetMapping("/list")
-    public ResponseEntity<?> list() {
-        List<PlantModel> plants = this.iPlantService.findAll();
-        return new ResponseEntity<>(plants, HttpStatus.OK);
+    // Controlador para obtener todas las plantas
+    @GetMapping
+    public ResponseEntity<List<PlantModel>> getAllPlants() {
+        return ResponseEntity.ok(plantService.findAll());
     }
+
+    // Controlador para crear una nueva planta
+    @PostMapping
+    public ResponseEntity<PlantModel> createPlant(@RequestBody Map<String, Object> payload) {
+        String nombre = (String) payload.get("nombre");
+        Long paisId = Long.valueOf(payload.get("paisId").toString());
+        PlantModel plant = plantService.createPlant(nombre, paisId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(plant);
+    }
+
 }
