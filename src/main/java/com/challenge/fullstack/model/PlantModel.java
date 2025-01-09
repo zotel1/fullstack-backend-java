437f87492ae6672e03282ded1031e40085db4c5a
zotel1 <zotelsigel@gmail.com>
2025-01-09 17:32:38 -0300

    update, plantModel

--- src/main/java/com/challenge/fullstack/model/PlantModel.java
+++ src/main/java/com/challenge/fullstack/model/PlantModel.java
@@ -1,10 +1,27 @@
 package com.challenge.fullstack.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlantModel {
-    Integer plant_id;
-    String name;
-    String category;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "pais_id", nullable = false)
+    private Country pais;
+
+    private Integer cantidadLecturas;
+    private Integer alertasMedias;
+    private Integer alertasRojas;
 }
