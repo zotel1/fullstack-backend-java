28623ef7f9753c2b21f62957b7a7ab585b9aa257
zotel1 <zotelsigel@gmail.com>
2025-01-09 17:37:09 -0300

    update, repository IPlantRepository

--- src/main/java/com/challenge/fullstack/repository/IPlantRepository.java
+++ src/main/java/com/challenge/fullstack/repository/IPlantRepository.java
@@ -1,9 +1,15 @@
 package com.challenge.fullstack.repository;
 
 import com.challenge.fullstack.model.PlantModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface IPlantRepository {
-    public List<PlantModel> findAll();
+//public interface IPlantRepository {
+  //  public List<PlantModel> findAll();
+//}
+
+@Repository
+public interface IPlantRepository extends JpaRepository<PlantModel, Long> {
 }
--- src/main/java/com/challenge/fullstack/repository/PlantRepository.java
+++ src/main/java/com/challenge/fullstack/repository/PlantRepository.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public class PlantRepository implements  IPlantRepository{
+//@Repository
+//public class PlantRepository implements  IPlantRepository{
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+  //  @Autowired
+    //private JdbcTemplate jdbcTemplate;
 
-    @Override
-    public List<PlantModel> findAll() {
-        String SQL = "SELECT * FROM plants";
-        return  jdbcTemplate.query(SQL, BeanPropertyRowMapper.newInstance(PlantModel.class));
-    }
-}
+  //  @Override
+//    public List<PlantModel> findAll() {
+  //      String SQL = "SELECT * FROM plants";
+    //    return  jdbcTemplate.query(SQL, BeanPropertyRowMapper.newInstance(PlantModel.class));
+  //  }
+//}
