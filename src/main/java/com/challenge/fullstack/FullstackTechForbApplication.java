8971858900db505187b1283a651ca923a11c4831
zotel1 <zotelsigel@gmail.com>
2025-01-10 16:37:08 -0300

    update, v6

--- src/main/java/com/challenge/fullstack/FullstackTechForbApplication.java
+++ src/main/java/com/challenge/fullstack/FullstackTechForbApplication.java
@@ -2,8 +2,11 @@ package com.challenge.fullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.challenge.fullstack.repository")
+
 public class FullstackTechForbApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/challenge/fullstack/repository/UserRepository.java
+++ src/main/java/com/challenge/fullstack/repository/UserRepository.java
@@ -7,18 +7,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public class UserRepository implements IUserRepository{
 
-  //  @Autowired
-    //private JdbcTemplate jdbcTemplate;
-
-  //  @Override
- //   public UserModel findByName(String user) {
-   //     String SQL = "SELECT * FROM users WHERE name = ?";
-     //   return jdbcTemplate.queryForObject(SQL, new Object[]{user},
-       //         new BeanPropertyRowMapper<>(UserModel.class));
-   // }
   @Repository
   public interface UserRepository extends JpaRepository<UserModel, Long> {
       UserModel findByName(String name);
