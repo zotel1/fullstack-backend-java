6f290f5ae18b1c22431aea8ce71f76e99b1d3d31
zotel1 <zotelsigel@gmail.com>
2025-01-10 01:43:02 -0300

    update, empezamos a utilizar getter y setter ya que lombok no me estaria funcionando

--- src/main/java/com/challenge/fullstack/dto/AuthRequestDto.java
+++ src/main/java/com/challenge/fullstack/dto/AuthRequestDto.java
@@ -2,8 +2,29 @@ package com.challenge.fullstack.dto;
 
 import lombok.Data;
 
-@Data
+//@Data
+//public class AuthRequestDto {
+  //  private String user;
+  //  private String password;
+//}
+
 public class AuthRequestDto {
     private String user;
     private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/challenge/fullstack/model/UserModel.java
+++ src/main/java/com/challenge/fullstack/model/UserModel.java
@@ -1,11 +1,59 @@
 package com.challenge.fullstack.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class UserModel {
-    Integer user_id;
-    String name;
-    String password;
-    String role;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+    private String name;
+    private String password;
+    private String role;
+
+    public UserModel(Integer user_id, String name, String password, String role) {
+        this.user_id = user_id;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
 }
