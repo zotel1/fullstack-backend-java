a2431ad2d3541a4520c1069a2d25cba9042cf3e1
zotel1 <zotelsigel@gmail.com>
2025-01-10 15:03:46 -0300

    update dependency

--- src/main/java/com/challenge/fullstack/config/SecurityConfig.java
+++ src/main/java/com/challenge/fullstack/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.challenge.fullstack.config;
 
 import com.challenge.fullstack.filter.JwtRequestFilter;
+import com.challenge.fullstack.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,9 @@ public class SecurityConfig {
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
