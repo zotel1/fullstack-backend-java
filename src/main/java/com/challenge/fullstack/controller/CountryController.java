543c8a1e9d624f8678431091af069f20973028a2
zotel1 <zotelsigel@gmail.com>
2025-01-13 02:04:02 -0300

    update jwt6

--- src/main/java/com/challenge/fullstack/controller/CountryController.java
+++ src/main/java/com/challenge/fullstack/controller/CountryController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @SecurityRequirement(name = "bearer-key")
-@RequestMapping("/api/v1/countries")
+@CrossOrigin("*") // Permite solicitudes desde cualquier origen
 @RestController
-@Controller
+@RequestMapping("/api/v1/countries")
 public class CountryController {
 
     private final CountryRepository countryRepository;
@@ -24,7 +25,7 @@ public class CountryController {
         this.countryRepository = countryRepository;
     }
 
-    @GetMapping
+    @GetMapping("/list") // Especificamos una subruta clara
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<Country>> getAllCountries() {
         List<Country> countries = countryRepository.findAll();
--- src/main/java/com/challenge/fullstack/controller/PlantController.java
+++ src/main/java/com/challenge/fullstack/controller/PlantController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.relational.core.sql.In;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,29 +18,28 @@ import java.util.List;
 import java.util.Map;
 
 @SecurityRequirement(name = "bearer-key")
-@Controller
 @CrossOrigin("*")
 @RestController
-@RequestMapping("api/v1/plants")
+@RequestMapping("/api/v1/plants")
 public class PlantController {
 
     @Autowired
     PlantService plantService;
 
-    // Controlador para obtener todas las plantas
-    @GetMapping
-    @SecurityRequirement(name = "bearer-key")
+    @GetMapping("/list") // Subruta específica para obtener todas las plantas
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<PlantModel>> getAllPlants() {
         return ResponseEntity.ok(plantService.findAll());
     }
 
     // Controlador para crear una nueva planta
-    @PostMapping
+    @PostMapping("/create")
     @Operation(summary ="Cree una nueva planta", description = "Agregue una nueva planta al sistema con un pais selecto")
     @ApiResponses({
             @ApiResponse(responseCode = "201", description = "Planta creada"),
             @ApiResponse(responseCode = "400", description = "Entrada invalida")
     })
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<PlantModel> createPlant(@RequestBody Map<String, Object> payload) {
         String nombre = (String) payload.get("nombre");
         Long paisId = Long.valueOf(payload.get("paisId").toString());
@@ -47,12 +47,13 @@ public class PlantController {
         return ResponseEntity.status(HttpStatus.CREATED).body(plant);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     @Operation(summary = "Actualice una planta", description = "Actualice una planta con su respectivo pais")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Planta actualizada"),
             @ApiResponse(responseCode = "404", description = "Planta no encontrada")
     })
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<PlantModel> updatePlant(
             @PathVariable Long id,
             @RequestBody Map<String, Object> payload) {
@@ -66,7 +67,8 @@ public class PlantController {
         return ResponseEntity.ok(updatePlant);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<Void> deletePlant(@PathVariable Long id) {
         plantService.deletePlant(id);
         return ResponseEntity.noContent().build();
