48d58ed2fbd646d9ef5ff1599cd3e38b9eadc6fc
zotel1 <zotelsigel@gmail.com>
2025-01-13 23:22:50 -0300

    update

--- src/main/java/com/challenge/fullstack/controller/PlantController.java
+++ src/main/java/com/challenge/fullstack/controller/PlantController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
