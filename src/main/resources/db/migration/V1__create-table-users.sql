28b42bce1f2523517160cee54fffc9c404be6324
zotel1 <zotelsigel@gmail.com>
2025-01-14 01:53:00 -0300

    update, migrations delete one coma

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -2,5 +2,5 @@ create table users (
     user_id bigint not null auto_increment primary key,
     name varchar(100) not null,
     password varchar(255) not null,
-    role varchar(50) not null,
+    role varchar(50) not null
    );
--- src/main/resources/db/migration/V2__create-table-country.sql
+++ src/main/resources/db/migration/V2__create-table-country.sql
@@ -1,5 +1,5 @@
 create table country (
     id bigint not null auto_increment primary key,
     nombre varchar(100) not null,
-    flag_url varchar(255),
+    flag_url varchar(255)
 );
