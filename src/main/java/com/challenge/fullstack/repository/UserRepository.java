f79ca48b244cb90b2e8e9d2d2d26cc1d4e7c3e0a
zotel1 <zotelsigel@gmail.com>
2025-01-14 03:20:14 -0300

    update

--- src/main/java/com/challenge/fullstack/repository/UserRepository.java
+++ src/main/java/com/challenge/fullstack/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.challenge.fullstack.repository;
 
 import com.challenge.fullstack.model.UserModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 
