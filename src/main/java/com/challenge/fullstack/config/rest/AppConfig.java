7250fc68e022e6afb0b28e28f4bdc17c46b576c9
zotel1 <zotelsigel@gmail.com>
2025-01-16 00:39:43 -0300

    update appConfig 2

--- src/main/java/com/challenge/fullstack/config/rest/AppConfig.java
+++ src/main/java/com/challenge/fullstack/config/rest/AppConfig.java
@@ -5,10 +5,10 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,9 +27,9 @@ public class AppConfig {
 
         // Configuración de tiempos de espera
         RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectTimeout(30000) // Tiempo de conexión: 30 segundos
-                .setResponseTimeout(60000) // Tiempo de espera de respuesta: 60 segundos
-                .setConnectionRequestTimeout(30000) // Tiempo para solicitar una conexión: 30 segundos
+                .setConnectTimeout(Timeout.ofSeconds(30)) // Tiempo de conexión: 30 segundos
+                .setResponseTimeout(Timeout.ofSeconds(60)) // Tiempo de espera de respuesta: 60 segundos
+                .setConnectionRequestTimeout(Timeout.ofSeconds(30)) // Tiempo para solicitar una conexión: 30 segundos
                 .build();
 
         // Configuración del cliente HTTP
