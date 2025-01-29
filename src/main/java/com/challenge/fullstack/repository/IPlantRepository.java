392b93050a73e87c1105cffa1bb4e3edbb20e79e
zotel1 <zotelsigel@gmail.com>
2025-01-29 11:36:46 -0300

    adaptamos el repository ya que en los booleanos en los @Query postgreSQL utiliza el valor en mayusculas, en este caso TRUE

--- src/main/java/com/challenge/fullstack/repository/IPlantRepository.java
+++ src/main/java/com/challenge/fullstack/repository/IPlantRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface IPlantRepository extends JpaRepository<PlantModel, Long> {
 
-    @Query("SELECT COUNT(p) FROM PlantModel p WHERE p.readingsOk = true")
+    @Query("SELECT COUNT(p) FROM PlantModel p WHERE p.readingsOk = TRUE")
     int countByReadingsOk();
 
 
@@ -24,13 +24,13 @@ public interface IPlantRepository extends JpaRepository<PlantModel, Long> {
     int countByRedAlerts();
 
     // Esta consulta debe coincidir con el atributo en PlantModel. Si disabled no existe, elimínala o corrige.
-    @Query("SELECT COUNT(p) FROM PlantModel p WHERE p.disabled = true")
+    @Query("SELECT COUNT(p) FROM PlantModel p WHERE p.disabled = TRUE")
     int countByDisabledSensors();
 
     // Añadimos los metodos para filtrar las plantas por el usuario creador
     @Query("SELECT p FROM PlantModel p WHERE p.createdBy.user_id = :userId")
     List<PlantModel> findByCreatedBy(@Param("userId") Long userId);
 // Permitimos a los administradores ver todas las plantas
-    @Query("SELECT p FROM PlantModel p WHERE p.createdBy.user_id = :userId OR :isAdmin = true")
+    @Query("SELECT p FROM PlantModel p WHERE p.createdBy.user_id = :userId OR :isAdmin = TRUE")
     List<PlantModel> findPlantsForUser(@Param("userId") Long userId, @Param("isAdmin") boolean isAdmin);
 }
