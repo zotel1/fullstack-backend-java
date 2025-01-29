f55db2b511455967b2f0f3d719cbcde4633798b0
zotel1 <zotelsigel@gmail.com>
2025-01-29 12:17:43 -0300

    Creamos la clase JpaConfig para dorzar la configuracion de JPA

---
+++ src/main/java/com/challenge/fullstack/config/JpaConfig.java
@@ -0,0 +1,33 @@
+package com.challenge.fullstack.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class JpaConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.challenge.fullstack.model") // Ajusta con el paquete donde tienes tus entidades
+                .persistenceUnit("default")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
