5aaeb0c381ef5db3a9884b78d0b22d2582315923
zotel1 <zotelsigel@gmail.com>
2025-01-30 05:34:41 -0300

    volvemos a crear el target ya que al parecer estaba basado en mysql

--- target/classes/db/migration/V1__create-table-users.sql
+++ target/classes/db/migration/V1__create-table-users.sql
@@ -1,6 +1,6 @@
 create table users (
-    user_id bigint not null auto_increment primary key,
-    username varchar(100) not null,
+    user_id serial primary key,
+    username varchar(100) not null unique,
     password varchar(255) not null,
     role varchar(50) not null
    );
