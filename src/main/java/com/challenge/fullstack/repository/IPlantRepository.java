49e3d1e77476dc30fac271f178fdfd2380dcf01c
zotel1 <zotelsigel@gmail.com>
2025-02-03 20:05:08 -0300

    con este metodo agregado en IPlantRepository permitimos obtener todas las plantas paginadas con Pageable de Spring Data JPA

--- src/main/java/com/challenge/fullstack/repository/IPlantRepository.java
+++ src/main/java/com/challenge/fullstack/repository/IPlantRepository.java
@@ -1,6 +1,8 @@
 package com.challenge.fullstack.repository;
 
 import com.challenge.fullstack.model.PlantModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -33,4 +35,8 @@ public interface IPlantRepository extends JpaRepository<PlantModel, Long> {
 // Permitimos a los administradores ver todas las plantas
     @Query("SELECT p FROM PlantModel p WHERE p.createdBy.user_id = :userId OR :isAdmin = true")
     List<PlantModel> findPlantsForUser(@Param("userId") Long userId, @Param("isAdmin") boolean isAdmin);
+
+    //Este metodo permite obtener las plantas paginadas con Pageable de Spring Data JPA
+    @Query("SELECT p FROM PlantModel p WHERE p.createdBy.user_id = :userId OR :isAdmin = true")
+    Page<PlantModel> findPlantsForUserPaginated(@Param("userId") Long userId, @Param("isAdmin") boolean isAdmin, Pageable pageable);
 }
